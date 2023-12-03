8484a0892f52427cae415d4b10fa380577243a06
yusufolola <yusufoyesanmi@yahoo.com>
2023-12-03 11:17:18 -0800

    more to come

--- interact.c
+++ interact.c
@@ -6,5 +6,23 @@
  */
 void interact(void)
 {
-	
+	char *line, **args;
+	int status = -1;
+
+	do {
+		/* print the prompt*/
+		gimme_com();
+		/* read the input and save it in 'line' */
+		line = read_input();
+		/* tokenize the input save it in 'args' */
+		args = tok_line(line);
+		/* use the return of the execve to know if successful*/
+		status = args_exec(args);
+		free(line);
+		free(args);
+		if (status >= 0)
+		{
+			exit(status);
+		} while (status == -1);
+	}
 }
