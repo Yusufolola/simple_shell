24275d9f4453e19a1bdf5adffec7d30fac74761d
wahab <wabsid01@gmail.com>
2023-12-13 00:51:19 -0800

    bettty

--- my_printf.c
+++ my_printf.c
@@ -1,14 +1,13 @@
-/*
- * @print_code: function for printing code in place of printf
+/**
+ * print_code - function for printing code in place of printf
  * @receive: string of character to be assed to the function
  *
- * */
-
+ */
 
 #include "shell.h"
 
 void print_code(const char *receive)
 {
-	write(STDOUT_FILENO, &receive, _strlen((char*)receive));
+	write(STDOUT_FILENO, &receive, _strlen((char *)receive));
 }
 
